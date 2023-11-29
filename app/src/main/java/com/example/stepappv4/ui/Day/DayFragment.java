afa7eab71186fd81dbbe955ae0967a6e959dd1d7
ChaofanHu <chaofan.hu@usi.ch>
2023-11-29 19:40:02 +0100

    calory ui layout9

--- app/src/main/java/com/example/stepappv4/ui/Day/DayFragment.java
+++ app/src/main/java/com/example/stepappv4/ui/Day/DayFragment.java
@@ -51,23 +51,11 @@ public class DayFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        // Create column chart
-        try {
-            anyChartView = root.findViewById(R.id.hourBarChart);
-            anyChartView.setProgressBar(root.findViewById(R.id.loadingBar));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        Cartesian cartesian = createColumnChart();
-        anyChartView.setBackgroundColor("#00000000");
-        anyChartView.setChart(cartesian);
+        View rootView = inflater.inflate(R.layout.fragment_day, container, false);
 
 
-        return root;
+
+        return rootView;
     }
 
     @Override
@@ -76,66 +64,5 @@ public class DayFragment extends Fragment {
         binding = null;
     }
 
-    public Cartesian createColumnChart(){
-        for (int i = 0; i < 7; i++) {
-            last7Days[i] = sdf.format(calendar.getTime());
-            calendar.add(Calendar.DAY_OF_MONTH, -1);
-        }
-        //***** Read data from SQLiteDatabase *********/
-        // TODO 1 (YOUR TURN): Get the map with hours and number of steps for today
-        //  from the database and assign it to variable stepsByHour
-        stepsByHour = StepAppOpenHelper.loadStepsByDay(getContext(), last7Days);
-        // TODO 2 (YOUR TURN): Creating a new map that contains hours of the day from 0 to 23 and
-        //  number of steps during each hour set to 0
-        Map<String, Integer> graph_map = new TreeMap<>();
-        for(int i =0; i<=6; i++){
-            graph_map.put(last7Days[i], 0); }
-
-        // TODO 3 (YOUR TURN): Replace the number of steps for each hour in graph_map
-        //  with the number of steps read from the database
-        graph_map.putAll(stepsByHour);
-
-        //***** Create column chart using AnyChart library *********/
-        // TODO 4: Create and get the cartesian coordinate system for column chart
-        Cartesian cartesian = AnyChart.column();
-
-        // TODO 5: Create data entries for x and y axis of the graph
-        List<DataEntry> data = new ArrayList<>();
-
-        for (Map.Entry<String,Integer> entry : graph_map.entrySet())
-            data.add(new ValueDataEntry(entry.getKey(), entry.getValue()));
-
-        // TODO 6: Add the data to column chart and get the columns
-        Column column = cartesian.column(data);
-
-        //***** Modify the UI of the chart *********/
-        // TODO 7 (YOUR TURN): Change the color of column chart and its border
-        column.fill("#1EB980");
-        column.stroke("#1EB980");
-
-
-        // TODO 8: Modifying properties of tooltip
-        column.tooltip()
-                .titleFormat("At day: {%X}")
-                .format("{%Value} Steps")
-                .anchor(Anchor.RIGHT_BOTTOM);
-
-        // TODO 9 (YOUR TURN): Modify column chart tooltip properties
-
-
-        // Modifying properties of cartesian
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-        cartesian.interactivity().hoverMode(HoverMode.BY_X);
-        cartesian.yScale().minimum(0);
-
-
-        // TODO 10 (YOUR TURN): Modify the UI of the cartesian
-        cartesian.yAxis(0).title("Number of steps");
-        cartesian.xAxis(0).title("Day");
-        cartesian.background().fill("#00000000");
-        cartesian.animation(true);
-
-        return cartesian;
-    }
 
 }
