5d632969165cc395f4f3ee91eefe79cf11a67e1e
RossellGustav <zty981015@outlook.com>
2023-12-12 23:10:21 +0100

    1232434454

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -57,5 +58,5 @@ dependencies {
     implementation("org.apache.commons:commons-collections4:4.4")
     implementation("org.apache.commons:commons-compress:1.21")
     implementation("org.apache.commons:commons-math3:3.6.1")
-    implementation ("com.github.warkiz.widget:indicatorseekbar:1.2.9")
+
 }
