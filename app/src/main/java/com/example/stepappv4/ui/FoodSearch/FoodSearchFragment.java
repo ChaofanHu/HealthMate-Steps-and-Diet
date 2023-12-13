1134a04daea02e2239b26e8e7ea39c1d39b15dcf
RossellGustav <zty981015@outlook.com>
2023-12-13 22:03:27 +0100

    888888888

--- app/src/main/java/com/example/stepappv4/ui/FoodSearch/FoodSearchFragment.java
+++ app/src/main/java/com/example/stepappv4/ui/FoodSearch/FoodSearchFragment.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.example.stepappv4.R;
 
 public class FoodSearchFragment extends Fragment {
@@ -12,9 +17,23 @@ public class FoodSearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_food_search, container, false);
+
+        // 假设您有一个按钮用于触发导航
+        ImageButton navigateButton = view.findViewById(R.id.backButton2);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToDayFragment();
+            }
+        });
+
+        return view;
+    }
+
+    private void navigateToDayFragment() {
+        NavController navController = NavHostFragment.findNavController(this);
+        navController.navigate(R.id.action_nav_food_search_to_nav_calorie);
     }
 }
-
 
--- app/src/main/java/com/example/stepappv4/ui/UserDetails/UserDetailsFragment.java
+++ app/src/main/java/com/example/stepappv4/ui/UserDetails/UserDetailsFragment.java
@@ -4,16 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.stepappv4.R;
 
 public class UserDetailsFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_age_weight, container, false);
+        View view = inflater.inflate(R.layout.fragment_age_weight, container, false);
+
+        // 假设您有一个按钮用于触发导航
+        ImageButton navigateButton = view.findViewById(R.id.backButton);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToDayFragment();
+            }
+        });
+
+        return view;
+    }
+
+    private void navigateToDayFragment() {
+        NavController navController = NavHostFragment.findNavController(this);
+        navController.navigate(R.id.action_nav_age_weight_to_nav_day);
     }
 }
