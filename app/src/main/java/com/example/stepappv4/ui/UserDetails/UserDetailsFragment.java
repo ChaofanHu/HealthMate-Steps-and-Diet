aecf318385f88c13e4fc290837d87aaad34f5268
RossellGustav <zty981015@outlook.com>
2023-12-15 16:23:25 +0100

    9999999999

--- app/src/main/java/com/example/stepappv4/ui/UserDetails/UserDetailsFragment.java
+++ app/src/main/java/com/example/stepappv4/ui/UserDetails/UserDetailsFragment.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -15,11 +16,58 @@ import com.example.stepappv4.R;
 
 public class UserDetailsFragment extends Fragment {
 
+    private TextView userAgeTextView;
+    private SeekBar ageSeekBar;
+    private TextView userWeightTextView;
+    private SeekBar weightSeekBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_age_weight, container, false);
+
+        userAgeTextView = view.findViewById(R.id.userAge);
+        ageSeekBar = view.findViewById(R.id.simpleSeekBarForAge);
+        userWeightTextView = view.findViewById(R.id.userWeight);
+        weightSeekBar = view.findViewById(R.id.simpleSeekBarForWeight);
+
+        // 设置年龄SeekBar的监听器
+        ageSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                userAgeTextView.setText(progress + " yrs");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // 不需要实现
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // 不需要实现
+            }
+        });
+
+        // 设置体重SeekBar的监听器
+        weightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                userWeightTextView.setText(progress + " kg");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // 不需要实现
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // 不需要实现
+            }
+        });
+
+        initializeSeekBar();
 
         // 假设您有一个按钮用于触发导航
         ImageButton navigateButton = view.findViewById(R.id.backButton);
@@ -37,4 +85,20 @@ public class UserDetailsFragment extends Fragment {
         NavController navController = NavHostFragment.findNavController(this);
         navController.navigate(R.id.action_nav_age_weight_to_nav_day);
     }
+
+    private void initializeSeekBar() {
+        // 假设初始年龄为25岁
+        int initialAge = 25;
+        ageSeekBar.setMax(100); // 最大年龄100
+        ageSeekBar.setProgress(initialAge); // 设置初始进度
+        userAgeTextView.setText(initialAge + " yrs");
+
+        // 假设初始体重为65kg
+        int initialWeight = 65;
+        weightSeekBar.setMax(300); // 最大体重300
+        weightSeekBar.setProgress(initialWeight); // 设置初始进度
+        userWeightTextView.setText(initialWeight + " kg");
+    }
 }
+
+
